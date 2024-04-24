57e852d754944ee349578de6b762cae315ce33f0
Myron <49134743+mmyyrroonn@users.noreply.github.com>
2024-04-24 17:32:40 +0800

    new query tab (#9)

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="flex place-content-center">
-      <h1 class="text-5xl font-bold text-center"> Twitter Summary for The Last Day </h1>
+      <h1 class="text-5xl font-bold text-center"> 更加易读的推特 </h1>
     </div>
     <TwitterTable></TwitterTable>
   </div>
