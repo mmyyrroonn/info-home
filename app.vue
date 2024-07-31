d065cdd4b0b29cc34a3af63c5248a052ec90cb64
myron <qiufan_glamour@hotmail.com>
2024-07-31 21:36:55 +0800

    add auto reply

--- app.vue
+++ app.vue
@@ -7,10 +7,14 @@
       <button @click="showLatestTwitter" class="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded">
         最新推特
       </button>
+      <button @click="showAutoReply" class="bg-teal-500 hover:bg-teal-600 text-white py-2 px-4 rounded">
+        自动回复
+      </button>
     </div>
     <div class="mt-4">
       <TwitterTable v-if="showTable" />
       <LatestTwitter v-if="showLatest" />
+      <AutoReply v-if="showAuto" />
     </div>
   </div>
 </template>
@@ -19,24 +23,32 @@
 export default {
   components: {
     TwitterTable: () => import('@/components/TwitterTable.vue'),
-    LatestTwitter: () => import('@/components/LatestTwitter.vue')
+    LatestTwitter: () => import('@/components/LatestTwitter.vue'),
+    AutoReply: () => import('@/components/AutoReply.vue')
   },
   data() {
     return {
       showTable: true,
-      showLatest: false
+      showLatest: false,
+      showAuto: false
     };
   },
   methods: {
     showTwitterTable() {
       this.showTable = true;
       this.showLatest = false;
+      this.showAuto = false;
     },
     showLatestTwitter() {
       this.showTable = false;
       this.showLatest = true;
+      this.showAuto = false;
+    },
+    showAutoReply() {
+      this.showTable = false;
+      this.showLatest = false;
+      this.showAuto = true;
     }
   }
 };
 </script>
-
